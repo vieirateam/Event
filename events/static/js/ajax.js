8893d53189d67f59a8b53df34bead13824bbbb21
User <vinivieiradossantos@gmail.com>
2018-06-29 21:28:59 -0300

    Pendency
    Apenas a 1ª aprovada

--- events/static/js/ajax.js
+++ events/static/js/ajax.js
@@ -10,17 +10,17 @@ $(function() {
         });
     }
 
-    if($('#url_talk') != null && $('#url_speaker') != null) {
-        $('#vt_talk_approve').click(function() {
-            let url_talk = $('#url_talk').attr('data-url');
+    if($('.url_talk') != null && $('.url_speaker') != null) {
+        $('.vt_talk_approve').click(function() {
+            let url_talk = $('.url_talk').attr('data-url');
             $.get(url_talk, function(response) {
                 console.log(response);
             });
             getJson();
             $(this).parent().hide();
         });
-        $('#vt_speaker_approve').click(function() {
-            let url_speaker = $('#url_speaker').attr('data-url');
+        $('.vt_speaker_approve').click(function() {
+            let url_speaker = $('.url_speaker').attr('data-url');
             $.get(url_speaker, function(response) {
                 console.log(response);
             });
