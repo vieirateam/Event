c4d5b2f82937580fe0a57b74d8679de05954244a
Wellinton Vieira <wellintonvieiradossantos@hotmail.com>
2018-07-03 09:42:50 -0300

    slide show

--- events/static/js/index.js
+++ events/static/js/index.js
@@ -1,62 +1,23 @@
 "use strict"
 
-let myIndex = 0;
 let slideIndex = 1;
-
-carousel();
 showDivs(slideIndex);
 
-function carousel() {
+function plusDivs(index) {
+    showDivs(slideIndex += index);
+}
+
+function showDivs(index) {
     let i;
-    let x = $(".mySlides");
-    
-    x.each(function(index) {
-        $(this).hide();
-    });
-    
-    myIndex++;
-    
-    if (myIndex > x.length) {
-        myIndex = 1
-    }
-
-    $(".mySlides:eq("+myIndex+")").show();
-    slideIndex = myIndex;  
-    showDivs(slideIndex);
-    setTimeout(carousel, 4000); 
-}
-
-function plusDivs(n) {
-    myIndex += n;
-    showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-    myIndex = n;
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-    let i;
-    let x = $(".mySlides");
-    let dots = $(".demo");
-    
-    if (n > x.length) {
+    let x = document.getElementsByClassName("show-slides");
+    if (index > x.length) {
         slideIndex = 1
-    }    
-    if (n < 1) {
-        slideIndex = x.length
     }
-    slideIndex -= 1;
-    
-    x.each(function(index) {
-        $(this).hide();
-    });
-    
-    dots.each(function(index) {
-        $(this).removeClass("w3-white");
-    })
-    
-    $(".mySlides:eq("+slideIndex+")").show();  
-    $(".demo:eq("+slideIndex+")").addClass("w3-white");
+    if (index < 1) {
+        slideIndex = x.length
+    };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
 }
