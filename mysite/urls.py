943d949039760b0ca89b9d87290c2eef5ce73148
Wellinton Vieira <wellintonvieiradossantos@hotmail.com>
2018-08-24 02:25:57 -0300

    new update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from mysite import settings
 from django.contrib import admin
 from django.contrib.auth import views
@@ -12,3 +13,9 @@ urlpatterns = [
     url(r'^register/$', events_views.register, name="register"), 
     url(r'', include('events.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# This enables static files to be served from the Gunicorn server
+# In Production, serve static files from Google Cloud Storage or an alternative
+# CDN
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
